4e5f10567ea1789476ea58bc9fe365d995df1033
doeunkim <doeuning@gmail.com>
2022-08-25 17:27:50 +0900

    test

--- server.js
+++ server.js
@@ -1,22 +1,29 @@
 // 1. express 호출
 const express = require("express");
-const cors = require("cors");
 const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const path = require("path");
+const corsOptions = {
+  origin: "*",
+  Credential: true,
+  withCredentials: true,
+};
 
 // 2. 포트 설정
 app.set("port", 4000);
 
 // 3. 공통 사용 미들웨어 장착
-app.use(cors());
-app.use((req, res) => {
-  res.header({
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Origin": "http://localhost:4000",
-    "Access-Control-Allow-Headers":
-      "Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization",
-  }); // 모든 도메인 허용
-});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+// app.use((req, res) => {
+//   res.header({
+//     "Access-Control-Allow-Origin": "*",
+//     "Access-Control-Allow-Headers":
+//       "Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization",
+//   }); // 모든 도메인 허용
+// });
 
 // 7. 생성된 서버가 포트를 리스닝
 app.listen(app.get("port"), () => {
